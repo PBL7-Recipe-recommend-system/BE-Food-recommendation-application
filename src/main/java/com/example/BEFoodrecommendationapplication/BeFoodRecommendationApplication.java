9c483ef577da059de22ea37b7b343afdd569b280
NhatChu <chunhat321123@gmail.com>
2024-04-16 13:13:25 +0700

    Init project

---
+++ src/main/java/com/example/BEFoodrecommendationapplication/BeFoodRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.example.BEFoodrecommendationapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeFoodRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeFoodRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BEFoodrecommendationapplication/BeFoodRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BEFoodrecommendationapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeFoodRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
