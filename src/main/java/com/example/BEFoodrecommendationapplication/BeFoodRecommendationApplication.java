b001173d2d951adfe4c912f321fc1c2338f88639
NhatChu <chunhat321123@gmail.com>
2024-04-16 13:32:16 +0700

    Init project

--- src/main/java/com/example/BEFoodrecommendationapplication/BeFoodRecommendationApplication.java
+++ src/main/java/com/example/BEFoodrecommendationapplication/BeFoodRecommendationApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeFoodRecommendationApplication {
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(BeFoodRecommendationApplication.class, args);
 	}
