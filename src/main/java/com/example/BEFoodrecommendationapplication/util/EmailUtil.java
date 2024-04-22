a735717786f17d6f2b02e051d316abc3fbbaddea
NhatChu <chunhat321123@gmail.com>
2024-04-22 08:48:56 +0700

    feat: Implement forgot password api

---
+++ src/main/java/com/example/BEFoodrecommendationapplication/util/EmailUtil.java
@@ -0,0 +1,46 @@
+package com.example.BEFoodrecommendationapplication.util;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailUtil {
+
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendOtpEmail(String email, String otp) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+        mimeMessageHelper.setTo(email);
+        mimeMessageHelper.setSubject("Verify OTP");
+        mimeMessageHelper.setText("""
+        <div>
+          <a href="http://localhost:8081/verify-account?email=%s&otp=%s" target="_blank">click link to verify</a>
+        </div>
+        """.formatted(email, otp), true);
+
+        javaMailSender.send(mimeMessage);
+    }
+
+    public void sendResetPasswordEmail(String email) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+        mimeMessageHelper.setTo(email);
+        mimeMessageHelper.setSubject("Reset password");
+        mimeMessageHelper.setText("""
+        <div>
+          <a href="http://localhost:8081/set-password?email=%s" target="_blank">click link to set your password.</a>
+        </div>
+        """.formatted(email), true);
+
+        javaMailSender.send(mimeMessage);
+    }
+}
